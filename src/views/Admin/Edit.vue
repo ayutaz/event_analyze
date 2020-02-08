74b3777eaff918314a06f019d9e426164e8a54b5
yuta <ka1357amnbpdr@gmail.com>
2020-02-09 01:04:42 +0900

    [add]商品の変更と削除ボタンの追加

--- src/views/Admin/Edit.vue
+++ src/views/Admin/Edit.vue
@@ -9,6 +9,10 @@
           <label>値段</label>
           <input type="number" v-model="item_price">
         </div>
+        <div class="ui centered grid">
+          <button class="ui positive button four wide column" type="button" @click="changeItem()">変更</button>
+          <button class="ui negative button four wide column" type="button" @click="deleteItem()">削除</button>
+        </div>
       </form>
     </div>
 </template>
@@ -25,12 +29,31 @@ export default {
   },
   created () {
     this.db = firebase.firestore()
+    let _this = this
     this.db.collection('items_index').doc(this.$route.params.id).onSnapshot(function (doc) {
       let datas = doc.data()
-      console.log(datas)
-      this.item_name = datas.item_name
-      this.item_price = datas.item_price
+      _this.item_name = datas.item_name
+      _this.item_price = datas.item_price
     })
+  },
+  methods: {
+    changeItem: function () {
+      this.db.collection('items_index').doc(this.$route.params.id).set({
+        item_name: this.item_name,
+        item_price: this.item_price
+      }).then(function () {
+        console.log('Document successfully written!')
+      }).catch(function (error) {
+        console.error('Error writing document: ', error)
+      })
+    },
+    deleteItem: function () {
+      this.db.collection('item_index').doc(this.$$route.params.id).delete().then(function () {
+        console.log('Document successfully deleted!')
+      }).catch(function (error) {
+        console.error('Error removing document: ', error)
+      })
+    }
   }
 }
 </script>
