b922b536b628f635ae1697112d14eea76573aee6
yuta <ka1357amnbpdr@gmail.com>
2020-02-09 01:23:10 +0900

    [add]履歴の性別と年齢の変更機能の実装

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -1,10 +1,23 @@
 <template>
     <div class="edit">
-        <div class="ui card">
-            <div class="content">
-                {{age}}
-            </div>
+      <form class="ui form">
+        <div class="field">
+          <label>年齢</label>
+          <input v-model="age">
         </div>
+        <div class="field ui grid">
+            <label>性別</label>
+            <div class="ui buttons">
+              <button class="ui button" @click="changeSex('男')" type="button">男性</button>
+              <button class="ui button" @click="changeSex('女')" type="button">女性</button>
+              <button class="ui button" @click="changeSex('その他')" type="button">その他</button>
+            </div>
+          </div>
+        <div class="ui centered aligned grid">
+          <button class="ui negative button four wide column" type="button" @click="deleteItem()">削除</button>
+          <button class="ui positive button four wide column" type="button" @click="changeItem()">変更</button>
+        </div>
+      </form>
     </div>
 </template>
 
@@ -37,6 +50,28 @@ export default {
         _this.items.push(item)
       })
     })
+  },
+  methods: {
+    changeItem: function () {
+      this.db.collection('historys').doc(this.$route.params.id).update({
+        age: this.age,
+        sex: this.sex
+      }).then(function () {
+        console.log('Document successfully written!')
+      }).catch(function (error) {
+        console.error('Error writing document: ', error)
+      })
+    },
+    deleteItem: function () {
+      this.db.collection('historys').doc(this.$$route.params.id).delete().then(function () {
+        console.log('Document successfully deleted!')
+      }).catch(function (error) {
+        console.error('Error removing document: ', error)
+      })
+    },
+    changeSex: function (sex) {
+      this.sex = sex
+    }
   }
 }
 </script>
